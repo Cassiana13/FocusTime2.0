b3b9d52399b6b59d093ba60bd477754c483d3910
Cassiana Caires <100693203+Cassiana13@users.noreply.github.com>
2024-02-19 21:08:55 -0300

    Add files via upload

---
+++ src/FocusTimer/sounds.js
@@ -0,0 +1,12 @@
+export const buttonPressAudioFlorest = new Audio("./assets/Floresta.wav");
+
+export const buttonPressAudioRain = new Audio("./assets/Chuva.wav");
+
+export const buttonPressAudioCoffee = new Audio("./assets/Cafeteria.wav");
+
+export const buttonPressAudioFirePlace = new Audio("./assets/Lareira.wav");
+
+buttonPressAudioFlorest.loop = true;
+buttonPressAudioRain.loop = true;
+buttonPressAudioCoffee.loop = true;
+buttonPressAudioFirePlace.loop = true;
